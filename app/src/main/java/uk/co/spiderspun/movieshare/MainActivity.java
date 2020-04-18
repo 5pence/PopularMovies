80dcf66b9e962bc92a4f365fd129d1f3d56f3e4b
spencer barriball <sbarriball@me.com>
2020-04-18 03:10:57 +0100

    add wiring of grid layout manager and recycler to activity main

--- app/src/main/java/uk/co/spiderspun/movieshare/MainActivity.java
+++ app/src/main/java/uk/co/spiderspun/movieshare/MainActivity.java
@@ -1,14 +1,27 @@
 package uk.co.spiderspun.movieshare;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView mRecyclerView;
+    private GridLayoutManager mGridLayoutManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // wire up recycle view to resource
+        mRecyclerView = (RecyclerView) findViewById(R.id.list_of_movies_rv);
+        // create grid layout manager
+        mGridLayoutManager = new GridLayoutManager(this, 2,
+                GridLayoutManager.VERTICAL, false);
+        // wire up the grid layout manager to recycler view
+        mRecyclerView.setLayoutManager(mGridLayoutManager);
+        mRecyclerView.setHasFixedSize(true);
     }
 }
